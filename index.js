f59e83f04e2f6fc255a93953960c7121897f8a15
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2024-02-12 00:35:58 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+var bestCustomer
+const leastFavoriteCustomer = 'George'
+
+function upperCaseCustomerName(){
+  return customerName = "BOB"
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "dan"
+}
